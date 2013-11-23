c1c55fb176fe0a465a040f05b3b85effbd291a02
Rahul Paliwal <rahulpaliwal172@gmail.com>
2013-11-24 04:37:52 +0530

    tested out the freemarker templating lib

--- src/prj/pingback/heartbeat/handler/PingbackHandler.java
+++ src/prj/pingback/heartbeat/handler/PingbackHandler.java
@@ -1,5 +1,8 @@
 package prj.pingback.heartbeat.handler;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import prj.httpApplication.RawHTTPResponse;
@@ -9,6 +12,11 @@ import prj.pingback.heartbeat.devicemanager.Device;
 import prj.pingback.heartbeat.devicemanager.DeviceManager;
 import prj.pingback.utils.ParseUtils;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PingbackHandler extends HTTPRequestHandler
@@ -20,6 +28,11 @@ public class PingbackHandler extends HTTPRequestHandler
     {
         _logger = LoggerFactory.getLogger(PingbackHandler.class);
         _deviceManager = deviceManager;
+
+        _deviceManager.registerDevice(new Device("abc", "1.3.4"));
+        _deviceManager.registerDevice(new Device("abcd", "1.3.6"));
+        _deviceManager.registerDevice(new Device("abcde", "1.3.5"));
+        _deviceManager.registerDevice(new Device("abcdef", "1.3.5"));
     }
 
     @Override
@@ -30,7 +43,37 @@ public class PingbackHandler extends HTTPRequestHandler
         {
             {
                 _logger.debug("response {}", _deviceManager.toString());
-                setBody(_deviceManager.toString());
+//                setBody(_deviceManager.toString());
+                Configuration cfg = new Configuration();
+
+                String output = null;
+                try
+                {
+                    Template template = cfg.getTemplate("heartbeat.ftl");
+
+                    // Build the data-model
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("message", "Hello World!");
+
+                    //List parsing
+                    List<String> countries = new ArrayList<>();
+                    countries.add("India");
+                    countries.add("United States");
+                    countries.add("Germany");
+                    countries.add("France");
+
+                    data.put("countries", countries);
+
+                    StringWriter stringWriter = new StringWriter();
+                    template.process(data, stringWriter);
+
+                    output = stringWriter.toString();
+                }
+                catch (IOException |TemplateException e)
+                {
+                    e.printStackTrace();
+                }
+                setBody(output);
             }
         };
     }
